be420965f759d8975323fadb10a66408a41bd0db
Michael Y <Michael Y>
2016-11-12 13:28:58 -0500

    remove ios

--- WebApp/js/app.js
+++ WebApp/js/app.js
@@ -20,8 +20,8 @@
 	};*/
  	portfolio.controller('mainController', function($http, $scope){
  		this.content = pageContent;
-		
 		this.houses = null;
+		this.houseJson = null;
 		$http.get('houses.json').then(function(res){
 			this.houses = res.data;
 		}.bind(this)); //need to bind to refer to this object
@@ -29,6 +29,45 @@
 		$http.get('0001.json').then(function(res){
 			this.houseJson = res.data;
 		}.bind(this)); //need to bind to refer to this object
+
+		// Initialize Firebase
+        var config = {
+            apiKey: "AIzaSyAYOjZx7LghW8XL6nSVeEtO3M66eHdwIQY",
+            authDomain: "fanniemae-efcae.firebaseapp.com",
+            databaseURL: "https://fanniemae-efcae.firebaseio.com",
+            storageBucket: "fanniemae-efcae.appspot.com",
+            messagingSenderId: "794391373641"
+        };
+        firebase.initializeApp(config);
+
+        //get elements
+        var uploader = $("#uploader")[0];
+        var fileButton = $("#fileButton")[0];
+
+        //Listen for Files
+        fileButton.addEventListener("change", function(e){
+            //get file
+            var file = e.target.files[0];
+            //create storage reference to upload
+            var storageRef = firebase.storage().ref('img/'+file.name);
+            //upload file
+            var task = storageRef.put(file);
+
+            //Update progress bar
+            task.on('state_changed', 
+                function progress(snapshot){
+                    var percentage = 100*snapshot.bytesTransferred / snapshot.totalBytes;
+                    uploader.value = percentage;
+                },
+                function error(err){
+
+                },
+                function complete(){
+                    alert('Upload Success!');
+                }
+            );
+        });
+
 		this.checkStatus = function(status){
 			if(status==1){
 				return "tab-good";
@@ -37,14 +76,17 @@
 				return "tab-bad";
 			}
 		}
-		this.updateJson = function(h){
+		this.updateJson = function(){
 			/*$http.get(this.item+'.json').then(function(res){
 				this.houseJson = res.data;
 			}.bind(this)); //need to bind to refer to this object
 			//console.log(this.houseJson);
 			*/
 			// Create a reference to the file we want to download
-			var tempJson = storageRef.child(h+'/');
+			h = this.item;
+			console.log(h);
+			var storageRef = firebase.storage().ref(h+'/');
+			var tempJson = this.storageRef.child(h+".json");
 
 			// Get the download URL
 			tempJson.getDownloadURL().then(function(url) {
